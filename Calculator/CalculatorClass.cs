2bc5df361a1e1a85917c39cb13968220164f6d77
ieva.montrimaite <ieva.montrimaite@W10-43H30J3>
2023-02-08 16:13:56 +0200

    Add project files.

---
+++ Calculator/CalculatorClass.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Calculator
+{
+    public class CalculatorClass
+    {
+        public static int SumInt(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+        public static double SumDouble3(double number1, double number2, double number3)
+        {
+            return (double)number1 + number2 + number3;
+        }
+        public static decimal SumDecimal3(decimal number1, decimal number2, decimal number3)
+        {
+            return (decimal)number1 + number2 + number3;
+        }
+        public static int Difference(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+        public static int Product(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+        public static double Qoutient(int number1, int number2)
+        {
+            return (double)number1 / number2;
+        }
+    }
+}
---
+++ UnitTests/CalculatorUnitTests.cs
@@ -0,0 +1,46 @@
+﻿using Calculator;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    // nera Main'o, tai tik biblioteka
+    public class CalculatorUnitTests
+    {
+        [Test]
+        public void Sum1and1()
+        {
+            int expectedResult = 2;
+
+            int actualResult = Calculator.CalculatorClass.SumInt(1, 1);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        // glaustesnis variantas
+        [Test]
+        public void Sum2And2()
+        { 
+            Assert.AreEqual(4, Calculator.CalculatorClass.SumInt(2, 2));
+        }
+
+        [Test]// netikslus duomenys
+        public void SumDouble01And01And01()
+        {
+            double expectedResult = 0.3; 
+
+            double actualResult = Calculator.CalculatorClass.SumDouble3(0.1, 0.1, 0.1);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test] // tiksliau su decimal
+        public void SumDecimal01And01And01()
+        {
+            decimal expectedResult = 0.3m;
+
+            decimal actualResult = Calculator.CalculatorClass.SumDecimal3(0.1m, 0.1m, 0.1m);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        } 
+    }
+}
