2821bb7a2b5ee790b3887621d44a40538843213d
imontrimaite <montrimaite.ieva@gmail.com>
2023-02-09 12:05:33 +0200

    Add solution to second task

---
+++ UnitTests/homework-0209-2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TodayIsWednesdayTest
+{
+    
+    public class TodayIsWednesdayUnitTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            // Get today's day of the week
+            DayOfWeek today = DateTime.Today.DayOfWeek;
+
+            // Verify that today's day of the week is equal to Wednesday
+            Assert.AreEqual(today, DayOfWeek.Wednesday);
+        }
+    }
+
+    internal class TestMethodAttribute : Attribute
+    {
+    }
+
+    internal class TestClassAttribute : Attribute
+    {
+    }
+}
