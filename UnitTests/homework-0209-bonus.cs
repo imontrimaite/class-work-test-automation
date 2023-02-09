ea9c29c5b1a1341116dd37c1c076d0df86312b14
imontrimaite <montrimaite.ieva@gmail.com>
2023-02-09 14:36:25 +0200

    Add solution to bonus task

---
+++ UnitTests/homework-0209-bonus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class EvenNumbersTest
+    {
+         [Test]
+         public void Test_That_There_Are_Four_Even_Numbers_Between_1_And_10()
+         {
+              int count = 0;
+
+              for (int i = 1; i <= 10; i++) // fails if we include 10, passes if i < 10
+              {
+                  if (i % 2 == 0)
+                  {
+                      count++;
+                  }
+              }
+              Assert.AreEqual(4, count);
+         }
+    }
+}
