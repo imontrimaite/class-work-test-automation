b727d6c1a440b249f4165bcdaf6b03defa6a3125
imontrimaite <montrimaite.ieva@gmail.com>
2023-02-09 10:11:44 +0200

    Add solution to first task

---
+++ UnitTests/homework-0209.cs
@@ -0,0 +1,18 @@
+﻿
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    internal class homework_0209
+    {
+        [Test]
+        public void CheckRemainder995DividedBy3()
+        { 
+            int expectedResult = 0;
+
+            int actualResult = 995 % 3;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
