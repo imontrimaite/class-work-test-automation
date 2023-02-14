f1ae7921908fb83cf245eafa95986e648f4a1ca1
imontrimaite <montrimaite.ieva@gmail.com>
2023-02-14 20:42:52 +0200

    Add code for Driver class and packages

--- SeleniumFramework/Driver.cs
+++ SeleniumFramework/Driver.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace SeleniumFramework
 {
-    internal class Driver
+    public class Driver
     {
+        private static IWebDriver driver;
+        public static void InitializeDriver()
+        {
+            driver = new ChromeDriver();
+        }
+        public static IWebDriver GetDriver()
+        {
+            return driver;
+        }
+        public static void OpenURL(string url)
+        {
+            driver.Url = url;// atidaro kaip su refresh
+
+            // driver.Navigate().GoToUrl(url); // islaikomas kontekstas, veiksmai, kurie galejo buti atlikti
+        }
+        public static void QuitDriver()
+        {
+            driver.Quit();
+        }
     }
 }
