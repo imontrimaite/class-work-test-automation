f4930968f420452d3513c5cfdd558031af973402
imontrimaite <montrimaite.ieva@gmail.com>
2023-02-19 14:16:50 +0200

    Add demo test for single checkbox

---
+++ SeleniumFramework/Pages/DemoQA/CheckboxDemoPage.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+
+namespace SeleniumFramework.Pages.DemoQA
+{
+    public class CheckboxDemoPage
+    {
+        private static string inputSingleCheckbox = "//*[@id='isAgeSelected']";
+        private static string fieldSingleCheckboxSuccessMessage = "//*[@id='txtAge']";
+
+        public static void Open()
+        {
+            Common.WindowPosition();
+            Common.MaximizeWindow();
+            Driver.OpenURL("https://demo.seleniumeasy.com/basic-checkbox-demo.html");
+        }
+
+        public static void ClickSingleCheckbox()
+        {
+            Common.ClickElement(inputSingleCheckbox);
+        }
+
+        public static string GetSingleCheckboxSuccessMessage()
+        {
+            return Common.GetElementText(fieldSingleCheckboxSuccessMessage);
+        }
+    }
+}
---
+++ SeleniumTests/DemoQA/CheckboxDemoTests.cs
@@ -0,0 +1,35 @@
+﻿
+using NUnit.Framework;
+using SeleniumFramework;
+using SeleniumFramework.Pages.DemoQA;
+
+namespace SeleniumTests.DemoQA
+{
+    internal class CheckboxDemoTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Driver.InitializeDriver();
+            CheckboxDemoPage.Open();
+        }
+
+        [Test]
+        public void SingleCheckbox()
+        {
+            string expectedMessage = "Success - Check box is checked";
+
+            CheckboxDemoPage.ClickSingleCheckbox();
+            string actualResult = CheckboxDemoPage.GetSingleCheckboxSuccessMessage();
+
+            Assert.AreEqual(expectedMessage, actualResult);
+        }
+
+        [TearDown]
+        public void TearDown()
+        { 
+            Driver.QuitDriver();
+        }
+
+    }
+}
